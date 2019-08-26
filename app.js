284195138950262f1b97be5391aa52560af7dff4
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-27 05:11:02 +0530

    APIlinks changed for herokku deployment

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var app = new Express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-Mongoose.connect('mongodb://localhost:27017/PersonDB');
-//Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
+//Mongoose.connect('mongodb://localhost:27017/PersonDB');
+Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
 
 ////////////////////////////////////////////////
 //define dataschema
@@ -48,8 +48,8 @@ app.get('/retrieveInfo',(req,res)=>{
 });
 
 ///get link to the retrievel API
-const retrieveDataApi = "http://localhost:3046/retrieveInfo"
-//const retrieveDataApi = "https://employeedb-jossin.herokuapp.com/retrieveInfo"
+//const retrieveDataApi = "http://localhost:3046/retrieveInfo"
+const retrieveDataApi = "https://persondb-jossin.herokuapp.com/retrieveInfo"
 
 ///call the API in a function to retieve the data
 app.get('/viewpersons',(req,res)=>{
@@ -67,7 +67,6 @@ app.get('/viewpersons',(req,res)=>{
 
 app.get('/searchByMobAPI/',(req,res)=>{
     var prsnmob = req.query.q;
-    console.log(prsnmob)
     formSchema.find({umob:prsnmob}, (error, data)=>{
         if(error){
             throw error;
@@ -78,20 +77,18 @@ app.get('/searchByMobAPI/',(req,res)=>{
 });
 
 //define API link
-const searchByNameAPILink = 'http://localhost:3046/searchByMobAPI'
-//const searchByNameAPILink = "https://employeedb-jossin.herokuapp.com/retrieveInfo"
+//const searchByNameAPILink = 'http://localhost:3046/searchByMobAPI'
+const searchByNameAPILink = "https://persondb-jossin.herokuapp.com/searchByMobAPI"
 
 //use in function to retrieve data
 app.post('/searchPerson',(req,res)=>{
     item = req.body.smob;
-    console.log(item);
     request(searchByNameAPILink+"/?q="+ item, (error, response, body)=>{
         if(error){
             throw error;
         }else{
-            //var data = JSON.parse(body);
-            console.log(body);
-            //res.send(data);
+            var data = JSON.parse(body);
+            res.send(data);
         } 
     });
 });
