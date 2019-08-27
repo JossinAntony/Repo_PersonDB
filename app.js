77e87d0c79632da69f4a4851072bca50e6f859b5
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-27 12:30:42 +0530

    paarameter passing in /saveInfo changed from req.params.body to req.query . each parameter.

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(function (req, res, next) {
     next();
 });
 
-//Mongoose.connect('mongodb://localhost:27017/PersonDB');
-Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
+Mongoose.connect('mongodb://localhost:27017/PersonDB');
+//Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
 
 ////////////////////////////////////////////////
 //define dataschema
@@ -43,15 +43,17 @@ const formSchema = Mongoose.model('formdetails',
 );
 ////
 //define save API upon save button
-app.post('/saveInfo',(req,res)=>{
+app.get('/saveInfo/',(req,res)=>{
+    console.log(req.query.uname);
     //var details = req.body;
-    var person = new formSchema(req.body);
+    var person = new formSchema({'uname':req.query.uname, 'umob':req.query.umob, 'umsg':req.query.umsg, 'umail':req.query.umail});
     var result = person.save((error, data)=>{
         if (error){
             throw error;
         }else{
             //res.send("<script>alert('New record created!')</script>");
             res.send(data);
+            console.log(data);
         }
     });
 });
@@ -69,8 +71,8 @@ app.get('/retrieveInfo',(req,res)=>{
 });
 
 ///get link to the retrievel API
-//const retrieveDataApi = "http://localhost:3046/retrieveInfo"
-const retrieveDataApi = "https://persondb-jossin.herokuapp.com/retrieveInfo"
+const retrieveDataApi = "http://localhost:3046/retrieveInfo"
+//const retrieveDataApi = "https://persondb-jossin.herokuapp.com/retrieveInfo"
 
 ///call the API in a function to retieve the data
 app.get('/viewpersons',(req,res)=>{
@@ -98,8 +100,8 @@ app.get('/searchByMobAPI/',(req,res)=>{
 });
 
 //define API link
-//const searchByNameAPILink = 'http://localhost:3046/searchByMobAPI'
-const searchByNameAPILink = "https://persondb-jossin.herokuapp.com/searchByMobAPI"
+const searchByNameAPILink = 'http://localhost:3046/searchByMobAPI'
+//const searchByNameAPILink = "https://persondb-jossin.herokuapp.com/searchByMobAPI"
 
 //use in function to retrieve data
 app.post('/searchPerson',(req,res)=>{
