cf336b8dd27cf2ceb63a90444ce3e59b93e5f415
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-27 12:33:04 +0530

    apilinks changed to heroku api links

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(function (req, res, next) {
     next();
 });
 
-Mongoose.connect('mongodb://localhost:27017/PersonDB');
-//Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
+//Mongoose.connect('mongodb://localhost:27017/PersonDB');
+Mongoose.connect('mongodb+srv://jossin:jossin@cluster0-arjkd.mongodb.net/test?retryWrites=true&w=majority'); //mongodb cloudatlas add, remener to change password
 
 ////////////////////////////////////////////////
 //define dataschema
@@ -71,8 +71,8 @@ app.get('/retrieveInfo',(req,res)=>{
 });
 
 ///get link to the retrievel API
-const retrieveDataApi = "http://localhost:3046/retrieveInfo"
-//const retrieveDataApi = "https://persondb-jossin.herokuapp.com/retrieveInfo"
+//const retrieveDataApi = "http://localhost:3046/retrieveInfo"
+const retrieveDataApi = "https://persondb-jossin.herokuapp.com/retrieveInfo"
 
 ///call the API in a function to retieve the data
 app.get('/viewpersons',(req,res)=>{
@@ -100,8 +100,8 @@ app.get('/searchByMobAPI/',(req,res)=>{
 });
 
 //define API link
-const searchByNameAPILink = 'http://localhost:3046/searchByMobAPI'
-//const searchByNameAPILink = "https://persondb-jossin.herokuapp.com/searchByMobAPI"
+//const searchByNameAPILink = 'http://localhost:3046/searchByMobAPI'
+const searchByNameAPILink = "https://persondb-jossin.herokuapp.com/searchByMobAPI"
 
 //use in function to retrieve data
 app.post('/searchPerson',(req,res)=>{
